e2c866d2084464d6607a4152d8a96c7bbe46072c
Hasnat Ali <hasnatali42@gmail.com>
2024-09-15 10:36:17 +0500

    Tailwind configured

--- components/main-header.tsx
+++ components/main-header.tsx
@@ -4,19 +4,29 @@ import logoImg from "@/assets/logo.png";
 
 export const MainHeader = () => {
   return (
-    <header>
-      <Link href="/">
-        <img src={logoImg.src} alt="A plate with food on it" />
-        NextLevel Food
+    <header className="flex flex-row justify-evenly items-center p-7">
+      <Link href="/" className="flex flex-row items-center gap-6">
+        <Image
+          src={logoImg}
+          alt="A plate with food on it"
+          width={64}
+          height={64}
+          priority
+        />
+        <p className="text-xl text-white font-bold uppercase">NextLevel Food</p>
       </Link>
 
       <nav>
-        <ul>
+        <ul className="flex flex-row gap-10">
           <li>
-            <Link href="/meals">Browse Meals</Link>
+            <Link href="/meals" className="text-xl text-white font-bold">
+              Browse Meals
+            </Link>
           </li>
           <li>
-            <Link href="/community">Foodies Community</Link>
+            <Link href="/community" className="text-xl text-white font-bold">
+              Foodies Community
+            </Link>
           </li>
         </ul>
       </nav>
